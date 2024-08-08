2129949193d8e48c6d81ddd9d2be2fddeb174eae
kiaagr012 <kianagrawal126@gmail.com>
2024-08-07 20:03:32 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#create a dictionary
+dict = {'color':'red', 'name':'Kian Agrawal', 'age':11}
+print(dict)
+
+#accessing values
+print(dict['name'])
+
+#get the list of keys and values
+print(dict.keys())
+print(dict.values())
+
+#create a list
+list = ['red','Kian Agrawal',11 ]
+print(list)
+print(list[2])
